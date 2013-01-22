6f9b08cfbbbb0d4f28042a6d25945d21fca9882c
Sampo Laurila <sampo.laurila@gmail.com>
2013-01-22 19:03:23 +0200

    TetrominoTest

--- Tetris/test/gameLogic/TetrominoTest.java
+++ Tetris/test/gameLogic/TetrominoTest.java
@@ -39,24 +39,71 @@ public class TetrominoTest {
     }
 
     /**
-     * Test of getTetromino method
+     * Tests for Tetromino initializer
      */
+    
     @Test
-    public void testGetTetromino() {
+    public void testTetrominoLine() {
         Tetromino instance = new Tetromino(0);
         int[][] expResult = {{0, 0}, {0, 1}, {0, 2}, {0, -1}};
         int[][] result = instance.getTetromino();
         assertArrayEquals(expResult, result);
     }
 
-    /**
-     * Test of getStyle method
-     */
     @Test
-    public void testGetStyle() {
-        Tetromino instance = new Tetromino(0);
-        int expResult = 0;
-        int result = instance.getStyle();
-        assertEquals(expResult, result);
+    public void testTetrominoSquare() {
+        Tetromino instance = new Tetromino(1);
+        int[][] expResult = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+        int[][] result = instance.getTetromino();
+        assertArrayEquals(expResult, result);
     }
+    
+    @Test
+    public void testTetrominoS() {
+        Tetromino instance = new Tetromino(2);
+        int[][] expResult = {{0, 0}, {1, 0}, {1, 1}, {0, -1}};
+        int[][] result = instance.getTetromino();
+        assertArrayEquals(expResult, result);
+    }
+    
+    @Test
+    public void testTetrominoZ() {
+        Tetromino instance = new Tetromino(3);
+        int[][] expResult = {{0, 0}, {-1, 0}, {-1, -1}, {0, 1}};
+        int[][] result = instance.getTetromino();
+        assertArrayEquals(expResult, result);
+    }
+     
+    @Test
+    public void testTetrominoT() {
+        Tetromino instance = new Tetromino(4);
+        int[][] expResult = {{0, 0}, {1, 0}, {0, 1}, {-1, 0}};
+        int[][] result = instance.getTetromino();
+        assertArrayEquals(expResult, result);
+    }
+    
+    @Test
+    public void testTetrominoL() {
+        Tetromino instance = new Tetromino(5);
+        int[][] expResult = {{0, 0}, {0, -1}, {0, 1}, {1, -1}};
+        int[][] result = instance.getTetromino();
+        assertArrayEquals(expResult, result);
+    }
+    
+    @Test
+    public void testTetrominoJ() {
+        Tetromino instance = new Tetromino(6);
+        int[][] expResult = {{0, 0}, {0, -1}, {0, 1}, {-1, -1}};
+        int[][] result = instance.getTetromino();
+        assertArrayEquals(expResult, result);
+    }
+    
+    @Test
+    public void testTetrominoNull() {
+        Tetromino instance = new Tetromino(7);
+        int[][] expResult = null;
+        int[][] result = instance.getTetromino();
+        assertArrayEquals(expResult, result);
+    }
+
 }
